f6b7226c6553dee42ef074f53e1a828a1567e980
Nihiliister <bencasti9031@gmail.com>
2025-12-05 22:29:18 -0300

    Add files via upload

---
+++ src/config/webConfig.java
@@ -0,0 +1,30 @@
+package com.example.kathub.kathub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            // Configuraciones CORS pueden ser añadidas aquí si es necesario
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/api/**") // solo los endpoints de API
+                        .allowedOrigins(
+                                        "http://localhost:5173",
+                                        "http://localhost:3000") // origen específico
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("Content-Type", "Authorization")
+                        .allowCredentials(false);
+            }
+        };
+    }
+    
+}
